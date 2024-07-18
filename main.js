5314881e2f1b6b8991df0307364f8d2ea1b366b8
JahadB <beylercahad@gmail.com>
2024-07-18 17:59:28 +0400

    ADD search box and button, some styling. First model completed

--- main.js
+++ main.js
@@ -1,29 +1,88 @@
-const API_KEY = "11111111111111111";//Change to your API key
+const API_KEY = "111111111111111111111";//Change to your API key
 
-//const btn = document.getElementById("btn");
+const btn = document.getElementById("btn");
 
-const city = prompt("Enter a city:");
+const form = document.getElementById("form");
+const userInput = document.getElementById("city");
 
+
+const infoContainer = document.getElementById("info");
+
+
+function info(infoContainer, address, temp, condition, description) {
+  let location = infoContainer.querySelector('.location');
+    if (!location) {
+      location = document.createElement("p");
+      location.className = 'location';
+      infoContainer.appendChild(location);
+    }
+    location.innerHTML = address;
+
+    let cityTemp = infoContainer.querySelector('.temp');
+    if (!cityTemp) {
+      cityTemp = document.createElement("p");
+      cityTemp.className = 'temp';
+      infoContainer.appendChild(cityTemp);
+    }
+    cityTemp.textContent = temp + " F";
+
+    let weatherCondition = infoContainer.querySelector('.condition');
+    if (!weatherCondition) {
+      weatherCondition = document.createElement("p");
+      weatherCondition.className = 'condition';
+      infoContainer.appendChild(weatherCondition);
+    }
+    weatherCondition.textContent = condition;
+
+    let weatherDescription = infoContainer.querySelector('.description');
+    if (!weatherDescription) {
+      weatherDescription = document.createElement("p");
+      weatherDescription.className = 'description';
+      infoContainer.appendChild(weatherDescription);
+    }
+    weatherDescription.textContent = description;
+}
+
+
+
+function cityWeather(address, temp, description, condition) {
+  this.address = address;
+  this.temp = temp;
+  this.condition = condition;
+  this.description = description
+
+  this.info = function() {
+      return `Temperature in ${this.address} is ${this.temp}F. ${this.description}. ${this.condition}`;
+  }
+
+
+
+}
+
+
+btn.addEventListener("click", () => {
+  if(userInput.value != ""){
+    getCity(userInput.value);
+    userInput.value = "";
+  }
+});
+
+function getCity(city) {
 fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=${API_KEY}`, {mode: 'cors'})
 .then(function(response) {
   return response.json();
 })
 .then(function(response) {
-    function cityWeather(address, temp, description, condition) {
-        this.address = address;
-        this.temp = temp;
-        this.condition = condition;
-        this.description = description
 
-        this.info = function() {
-            return `Temperature in ${this.address} is ${this.temp}F. ${this.description}. ${this.condition}`;
-        }
+    //const city1 = new cityWeather(response.resolvedAddress, response.currentConditions.temp,
+    //                              response.description, response.currentConditions.conditions);
 
-    }
-
-    const city1 = new cityWeather(response.resolvedAddress, response.currentConditions.temp, response.description, response.currentConditions.conditions);
-    console.log(city1.info());
+    info(infoContainer, response.resolvedAddress, response.currentConditions.temp,
+                        response.currentConditions.conditions, response.description);
+    //console.log(city1.info());
   })
 .catch(e => {
     console.log("checking",e)
   });
+}
+
